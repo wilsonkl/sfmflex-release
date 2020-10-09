415bc20abd68c2de0522d467ce92ff921a97c45e
Kyle Wilson <kwilson24@washcoll.edu>
2020-10-09 14:46:43 -0400

    starting to add the code

---
+++ code/deps.jl
@@ -0,0 +1,25 @@
+using Pkg
+
+Pkg.add("Glob")
+Pkg.add("HTTP")
+Pkg.add("Gumbo")
+Pkg.add("Cascadia")
+
+Pkg.add("SparseArrays")
+Pkg.add("LinearAlgebra")
+Pkg.add("SharedArrays")
+Pkg.add("StaticArrays")
+Pkg.add("Rotations")
+Pkg.add("IterativeSolvers")
+Pkg.add("Arpack")
+Pkg.add("LinearMaps")
+
+Pkg.add("PyPlot")
+Pkg.add("Printf")
+Pkg.add("LaTeXStrings")
+
+Pkg.add("HDF5")
+Pkg.add("JLD")
+Pkg.add("JSON")
+
+Pkg.add("BenchmarkTools")
